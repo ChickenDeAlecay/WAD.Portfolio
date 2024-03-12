9ab37932b5e0bd8d0c6afb35417ade4882fc85a3
ChickenDe <alecjrob@sky.com>
2024-03-12 14:03:53 +0000

    started containers

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+<div class="container">
   <form action="./auth.php" method="post">
     <div class="mb-3 lg-2">
       <label for="exampleInputEmail1" class="form-label">Username</label>
@@ -20,6 +21,7 @@
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </div>
 </body>
 
 </html>
